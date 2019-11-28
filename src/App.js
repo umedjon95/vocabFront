5cfe723c3b8072ab343dc4682deaeb5f21d08bcd
Umedjon Safarov <umedjon95@gmail.com>
2019-11-28 11:38:00 +0500

    improved

--- src/App.js
+++ src/App.js
@@ -3,39 +3,77 @@ import './App.css';
 import axios from 'axios';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {word: ''};
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+constructor(props) {
+   super(props)
+   this.state = {
+      cards: [],
+      word: ""
+   }
+   this.getcard = this.getcard.bind(this) 
+   this.getcards = this.getcards.bind(this) 
+   this.onChangeword = this.onChangeword.bind(this) 
 
-  handleChange(event) {
-    this.setState({word: event.target.value});
-  }
+}
 
-  handleSubmit(event) {
-    console.log('A name was submitted: ' + this.state.word);
+componentDidMount() {
+   this.getcards();
+}
 
-    axios.get(`http://localhost:8080/word/`, this.state.word)
-      .then(res => {
-     console.log(res);
-      })
+getcards() {
+   axios.get('http://localhost:8080/word') 
+   .then(res => {
+      this.setState({ cards:  res.data.payload });
+   })
+}
 
-  }
+getcard() {
+   axios.get('http://localhost:8080/word/' + this.state.word) 
+   .then(res => {
+      console.log(res);
+      
+      this.setState({ cards:  res.data.payload });
+   })
+}
 
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Name:
-          <input type="text" value={this.state.word} onChange={this.handleChange} />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
-    );
-  }
+onChangeword(event) {
+   this.setState ({word: event.target.value})
+}
+
+render() {
+   var renderTableData = (
+      this.state.cards.map (
+         (card, index) => {
+            const { id, word,  meaning } = card
+            return (
+               <tr key={id}>
+                  <td>{id}</td>
+                  <td>{word}</td>
+                  <td>{meaning}</td>
+               </tr>
+            )
+         }
+      )
+   )
+   return (
+      <div>
+         <h1 id='title'>Wordlist</h1>
+         <input onChange = {this.onChangeword} value = {this.state.word} />
+         <button onClick = {this.getcard.bind(this)}>Getcard</button>
+         <button onClick = {this.getcards.bind(this)}>Getcards</button>
+         <table id='cards'>
+            <tbody>
+               <tr>
+                  <th>id</th>
+                  <th>word</th>
+                  <th>meaning</th>
+               </tr>
+               { renderTableData }
+            </tbody>
+         </table>
+      </div>
+      )
+   }
 }
 
 export default App;
